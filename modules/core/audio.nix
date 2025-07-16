50b3c1c0fe5fd313325b1ac6074398a7e6518064
skylerfrags <quadfps@gmail.com>
2025-07-16 01:38:35 +0100

    altered audio settings to prevent bitcrushing/static

--- modules/core/audio.nix
+++ modules/core/audio.nix
@@ -1,10 +1,21 @@
 { config, pkgs, ... }:
 {
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     wireplumber.enable = true;
     pulse.enable = true;
     alsa.enable = true;
-    jack.enable = true;
+    alsa.support32Bit = true;
+    jack.enable = false;
+    extraConfig.pipewire = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 1024;
+        "default.clock.min-quantum" = 1024;
+        "default.clock.max-quantum" = 8192;
+      };
+    };
   };
 }
