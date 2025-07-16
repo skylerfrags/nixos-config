ed03889164f00e88292044bfacd9cb44bb4d1f1c
skylerfrags <quadfps@gmail.com>
2025-07-16 01:44:06 +0100

    enabled transparency for kitty

--- modules/home/terminal.nix
+++ modules/home/terminal.nix
@@ -4,7 +4,7 @@
   programs.kitty = {
     enable = true;
     settings = {
-      #background_opacity = 0.5;
+      background_opacity = 0.75;
       hide_window_decorations = false; # enable when using plasma
     };
   };
