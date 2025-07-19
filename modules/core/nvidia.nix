e1f1f80026c480401deccfd51f45a65e9ce11ce1
skylerfrags <quadfps@gmail.com>
2025-07-19 19:36:26 +0100

    added note to nvidia module

--- modules/core/nvidia.nix
+++ modules/core/nvidia.nix
@@ -8,7 +8,7 @@
     modesetting.enable = true;
     powerManagement.enable = true;
     powerManagement.finegrained = false;
-    open = false;
+    open = false; # my 3070 Ti should use the open drivers but every time ive used them on nix/arch/fedora etc it leads to an unusable system, idk why
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
