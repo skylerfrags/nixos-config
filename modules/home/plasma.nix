60acac8011897d84941e3fd0b8b96d4b02fb09e8
skylerfrags <quadfps@gmail.com>
2025-07-20 17:01:36 +0100

    plasma-manager: changed all fonts

--- modules/home/plasma.nix
+++ modules/home/plasma.nix
@@ -7,8 +7,28 @@
     };
 
     fonts = {
-      general = {
+      fixedWidth = {
         family = "JetBrains Mono Nerd Font";
+        pointSize = 10;
+      };
+      general = {
+        family = "JetBrains Mono";
+        pointSize = 10;
+      };
+      menu = {
+        family = "JetBrains Mono";
+        pointSize = 10;
+      };
+      small = {
+        family = "JetBrains Mono";
+        pointSize = 8;
+      };
+      toolbar = {
+        family = "JetBrains Mono";
+        pointSize = 10;
+      };
+      windowTitle = {
+        family = "JetBrains Mono";
         pointSize = 10;
       };
     };
