8314c3a6d0b06f486e96a762ed270afad31a757f
skylerfrags <quadfps@gmail.com>
2025-07-20 20:56:24 +0100

    plasma-manager: add OBS Websocket bind

--- modules/home/plasma.nix
+++ modules/home/plasma.nix
@@ -2,8 +2,11 @@
   programs.plasma = {
     enable = true;
 
-    shortcuts = {
-      "services/net.local.obs-cmd.desktop"."_launch" = "Meta+F12"; # obs-cmd Websocket for replay buffer
+    hotkeys.commands."obs-save-replay" = {
+      name = "OBS Save Replay";
+      key = "Meta+F12";
+      command = "obs-cmd replay save";
+      comment = "Save OBS replays using obs-cmd and websockets";
     };
 
     fonts = {
